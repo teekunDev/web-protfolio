b78eda674bf46bed404bc5d6db46f38efdebfe2a
teekunDev <teekun@teekun.dev>
2025-02-11 09:04:26 +0100

    Misc. Changes

---
+++ components/AboutMe/values.ts
@@ -0,0 +1,33 @@
+const AboutMe = {
+    en: "Hi, my name is Robin, and I’m a 20-year-old software developer based in Austria. I am a certified professional with a completed apprenticeship in software development and 4 years of hands-on experience in the field.\n" +
+        "\n" +
+        "My expertise lies in backend development, where I have strong skills in PHP and TypeScript.\n" +
+        "\n" +
+        "In addition, I am currently enhancing my skills in frontend development, focusing on TypeScript/JavaScript, HTML, and CSS, to grow into a versatile full-stack developer.\n" +
+        "\n" +
+        "I also have intermediate knowledge of C# and am dedicated to crafting efficient, scalable, and innovative solutions. Continuous learning is at the core of my journey as I strive to stay ahead in the dynamic world of technology.\n" +
+        "\n" +
+        "Let’s create something amazing together!",
+    de: "Hallo, mein Name ist Robin, ich bin 20 Jahre alt und komme aus Österreich. Ich bin ein ausgebildeter Facharbeiter mit einer abgeschlossenen Lehre im Bereich Softwareentwicklung und habe 4 Jahre praktische Berufserfahrung.\n" +
+        "\n" +
+        "Meine Expertise liegt in der Backend-Entwicklung, wobei ich über umfangreiche Kenntnisse in PHP und TypeScript verfüge.\n" +
+        "\n" +
+        "Derzeit erweitere ich meine Fähigkeiten in der Frontend-Entwicklung, mit einem Schwerpunkt auf TypeScript/JavaScript, HTML und CSS, um mich zu einem vielseitigen Full-Stack-Entwickler weiterzuentwickeln.\n" +
+        "\n" +
+        "Zusätzlich habe ich grundlegende Kenntnisse in C# und bin stets bestrebt, effiziente, skalierbare und innovative Lösungen zu entwickeln. Kontinuierliches Lernen steht für mich im Mittelpunkt, um mit den dynamischen Anforderungen der Technologiebranche Schritt zu halten.\n" +
+        "\n" +
+        "Lass uns gemeinsam etwas Großartiges schaffen!",
+}
+
+export const Languages = [
+    {
+        language: "🇩🇪 German",
+        languageCode: "de"
+    },
+    {
+        language: "🇺🇸 English",
+        languageCode: "en"
+    }
+]
+
+export default AboutMe
